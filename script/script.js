dd572ea68c4e6e4911dd6e7c1bb85a64e1a677ae
Vikto0004 <vikto0004@gmail.com>
2024-02-07 12:43:35 +0100

    Update

--- script/script.js
+++ script/script.js
@@ -1,31 +1,53 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+
+
+  let swiper = null; // Объявляем переменную swiper глобально
+
+  // Функция для инициализации или отключения Swiper в зависимости от размера экрана
+  function initSwiper() {
+      if (window.innerWidth <= 1290) {
+          // Проверяем ширину экрана (здесь 768px)
+          // Инициализируем Swiper, если его еще нет
+          if (!swiper) {
+              swiper = new Swiper('.swiper', {
+                  direction: 'horizontal',
+                  loop: true,
+                  pagination: {
+                      el: '.swiper-pagination',
+                  },
+                  navigation: {
+                      nextEl: '.swiper-button-next',
+                      prevEl: '.swiper-button-prev',
+                  },
+                  scrollbar: {
+                      el: '.swiper-scrollbar',
+                  },
+                  
+                      slidesPerView: 'auto',
+                  
+                      spaceBetween: 20,
+                      
+                      centeredSlides: true,
+                  
+                      slidesPerGroup: 1,
+                  
+                      initialSlide: 2, 
+              });
+          }
+      } else {
+          // Уничтожаем Swiper, если он был инициализирован и ширина экрана меньше 768px
+          if (swiper) {
+              swiper.destroy();
+              swiper = null;
+          }
+      }
+
+  }
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // Вызываем функцию при загрузке страницы и изменении размера окна
+  window.onload = function () {
+      initSwiper();
+  };
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-
-    slidesPerView: 'auto',
-
-    spaceBetween: 20,
-    
-    centeredSlides: true,
-
-    slidesPerGroup: 1,
-
-    initialSlide: 2, 
-  });
+  window.onresize = function () {
+      initSwiper();
+  };
