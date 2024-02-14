ddacf4947089c135438ba05ff9e8dacd5b050654
Vikto0004 <vikto0004@gmail.com>
2024-02-14 20:24:41 +0100

    Update

---
+++ script/mob-menu.js
@@ -0,0 +1,34 @@
+(() => {
+  const mobileMenu = document.querySelector(".js-menu-container");
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const menuLinks = document.querySelectorAll(".mob-menu-link"); // Виберіть всі посилання в меню
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", () => {
+      toggleMenu(); // Закриваємо меню при кліку на посилання
+    });
+  });
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
