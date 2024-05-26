48b76833910a77932697edb2bb153e0a2c9da85f
Vikto0004 <vikto0004@gmail.com>
2024-05-26 16:17:16 +0200

    Update

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,12 @@
 const elBtnOpenMenu = document.querySelector('.js-open-menu');
 const elMenuContainer = document.querySelector('.js-menu-container');
+const elBtnScrollUp = document.querySelector('.btn-scroll_up');
 
 elBtnOpenMenu.addEventListener('click', () => {
+  if (elBtnScrollUp.classList.contains('btn-scroll_up-active')) {
+    elBtnScrollUp.classList.remove('btn-scroll_up-active');
+  }
+
   document.body.style.overflow = 'hidden';
   elMenuContainer.classList.add('is-open');
   setTimeout(() => {
@@ -18,5 +23,26 @@ elMenuContainer.addEventListener('click', e => {
     setTimeout(() => {
       elMenuContainer.classList.remove('is-open');
     }, 600);
+    trackScroll();
   }
 });
+
+window.addEventListener('scroll', trackScroll);
+elBtnScrollUp.addEventListener('click', goTop);
+
+function trackScroll() {
+  const scrolled = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+  if (scrolled > coords) {
+    elBtnScrollUp.classList.add('btn-scroll_up-active');
+  } else {
+    elBtnScrollUp.classList.remove('btn-scroll_up-active');
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -75);
+    setTimeout(goTop, 0);
+  }
+}
